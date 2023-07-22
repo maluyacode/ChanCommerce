bb1b9b1840960a6093f58ad9093486ee09bc3216
maluyacode <adlawandavemerc98@gmail.com>
2023-07-22 10:58:08 +0800

    stock table

--- database/migrations/2023_07_22_024600_create_stocks_table.php
+++ database/migrations/2023_07_22_024600_create_stocks_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('stocks', function (Blueprint $table) {
-            $table->id();
+            $table->id('item_id');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
